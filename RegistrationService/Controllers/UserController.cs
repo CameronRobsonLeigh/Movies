50d9e6771de4d38d146f6eafb3a84cbd3e0bd5cc
CameronRobsonLeigh <101926007+CameronRobsonLeigh@users.noreply.github.com>
2023-07-15 15:38:16 +0100

    Feature/add registration service (#5)
    * Create Registration Service
    
    Basic Microservice Registration project.
    
    * Update Movies.sln

---
+++ RegistrationService/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RegistrationService.Data;
+using RegistrationService.Models;
+
+namespace RegistrationService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserServiceContext _context;
+
+        public UserController(UserServiceContext context)
+        {
+            _context = context;
+        }
+
+        [Route("/getUsers")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUser()
+        {
+            return await _context.User.ToListAsync();
+        }
+
+        [Route("/adduser")]
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.User.Add(user);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetUser", new { id = user.ID }, user);
+        }
+    }
+}
