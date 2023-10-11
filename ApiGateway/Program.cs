fd2b0b7c57c03e897d14d3ba14f68328df75dae4
CameronRobsonLeigh <Cameron.RobsonLeigh@outlook.com>
2023-10-11 20:22:42 +0100

    Update ApiGateway so that we allow CORS

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -5,10 +7,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
+
 builder.Services.AddOcelot(builder.Configuration);
 
-var app = builder.Build();
-await app.UseOcelot();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAnyOrigin", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
 
+var app = builder.Build();
+
+app.UseCors("AllowAnyOrigin");  // Use CORS middleware
+
+await app.UseOcelot();
 
 app.Run();
