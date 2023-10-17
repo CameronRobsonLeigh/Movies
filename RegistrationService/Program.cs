324c87922b4757d37e5d5ae7e15edca2408e5770
CameronRobsonLeigh <Cameron.RobsonLeigh@outlook.com>
2023-10-17 20:48:42 +0100

    whitesapce

--- RegistrationService/Program.cs
+++ RegistrationService/Program.cs
@@ -4,14 +4,14 @@ using RegistrationService.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<UserServiceContext>(options =>
-         options.UseSqlite(@"Data Source=user.db"));
+    options.UseSqlite(@"Data Source=user.db"));
 
 var app = builder.Build();
 
@@ -29,13 +29,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    
 }
+
 app.UseCors(x => x.AllowAnyHeader());
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
