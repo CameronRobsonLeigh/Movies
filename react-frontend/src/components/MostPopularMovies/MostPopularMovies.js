6b5b7aa60b839b4e38ade87107e28041514ced82
CameronRobsonLeigh <Cameron.RobsonLeigh@outlook.com>
2024-01-01 15:29:54 +0000

    Update call for 8001 port

--- react-frontend/src/components/MostPopularMovies/MostPopularMovies.js
+++ react-frontend/src/components/MostPopularMovies/MostPopularMovies.js
@@ -13,7 +13,7 @@ function GrabCatalogApiService() {
       } else {
         // Code specific to production environment
         console.log('Production Mode2');
-        apiGatewayUrl = '8001:/api/popular';
+        apiGatewayUrl = ':8001/api/popular';
       }
       
 
